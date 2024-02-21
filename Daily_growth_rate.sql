6543cf6b1a05844ef68e57507f6515a041cd5c2c
ckstrouse <160700057+ckstrouse@users.noreply.github.com>
2024-02-20 21:15:27 -0800

    Add files via upload

---
+++ Daily_growth_rate.sql
@@ -0,0 +1,10 @@
+SELECT
+    "date",
+    daily_sales,
+    LAG(daily_sales) OVER (ORDER BY "date") AS prev_day_sales,
+    (daily_sales - LAG(daily_sales) OVER (ORDER BY "date")) / LAG(daily_sales) OVER (ORDER BY "date") * 100 AS daily_growth_rate
+FROM (
+    SELECT "date", SUM("total") AS daily_sales
+    FROM markets
+    GROUP BY "date"
+) daily_sales;
